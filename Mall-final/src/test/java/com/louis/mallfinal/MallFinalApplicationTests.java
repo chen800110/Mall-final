b660f519eb66ed3d22bde23d3329f2ecf2fefb2d
commit mall <hilouis2000@gmail.com>
2024-10-05 15:48:23 +0800

    初始化spring boot product

---
+++ Mall-final/src/test/java/com/louis/mallfinal/MallFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.louis.mallfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
